9e0d6a6d2d50e58fa054e590697c3e18f08ed520
ranom <al.ivette.ramon@igualada.epiaedu.cat>
2025-05-27 09:15:26 +0200

    Create notes.js

---
+++ afinador-web/src/notes.js
@@ -0,0 +1,13 @@
+const A4 = 440;
+const noteNames = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+export function getNoteInfo(freq) {
+  const semitones = 12 * Math.log2(freq / A4);
+  const roundedSemitone = Math.round(semitones);
+  const noteIndex = (roundedSemitone + 69) % 12;
+  const noteName = noteNames[noteIndex];
+  const midiNote = roundedSemitone + 69;
+  const octave = Math.floor(midiNote / 12) - 1;
+  const noteFreq = A4 * Math.pow(2, (midiNote - 69) / 12);
+  return { name: `${noteName}${octave}`, freq: noteFreq };
+}
