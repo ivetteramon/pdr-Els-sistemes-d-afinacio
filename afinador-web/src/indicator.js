aa8e2135bd982276f2f92da34bff052c33a72b47
ranom <al.ivette.ramon@igualada.epiaedu.cat>
2025-06-01 09:29:37 +0200

    Update indicator.js

--- afinador-web/src/indicator.js
+++ afinador-web/src/indicator.js
@@ -1,7 +1,7 @@
-export function updateUI(indicatorElement, freq, targetFreq) {
-  const centsOff = 1200 * Math.log2(freq / targetFreq);
-  const maxCents = 50;
+export function updateIndicator(tuningIndicator, centsOff, maxCents = 50) {
   const position = Math.max(-1, Math.min(1, centsOff / maxCents));
+
   const percent = 50 + position * 50;
-  indicatorElement.style.left = `calc(${percent}% - 1px)`;
+
+  tuningIndicator.style.left = `calc(${percent}% - 1px)`;
 }
